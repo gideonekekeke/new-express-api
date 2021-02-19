c4ab99a675c9bb52fe1baeb4601b349981e131ac
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-02-19 11:36:41 +0100

    made a changes

--- server.js
+++ server.js
@@ -8,6 +8,23 @@ const port = 3490;
 // creating an object instance
 const app = express();
 
+//connecting to the mongodb atlass
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection
+  .once("open", () => {
+    console.log("connected successfully....");
+  })
+  .on("error", () => {
+    console.log("error occured");
+  });
+
 //enabling our responds to be returned in json format
 app.use(express.json());
 
